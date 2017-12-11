b3abd818be1fe7ac937d11666e4fb4b1ca709aea
Dominik Baron <domo.baron@aon.at>
2017-12-11 12:51:14 +0100

    backup push

---
+++ Avalon_Stats/Avalon_Stats/PlayerWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalon_Stats
+{
+    class PlayerWrapper
+    {
+        public Player Player { get; }
+
+        public String FirstName => Player.FirstName;
+
+        public String PlayerName => Player.PlayerName;
+
+
+
+        public PlayerWrapper(Player player)
+        {
+            Player = player;
+        }
+    }
+}
